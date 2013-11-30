1a1f8ae2d1c01f4f12fd32c813d6067e6bcf6b58
Jonas Bergler <jonas@bergler.name>
2013-11-30 02:15:34 +0100

    Initial Import

---
+++ README.md
@@ -0,0 +1,14 @@
+SpotifyAVRCP
+============
+
+Injects a media changed intent on track change to provide AVRCP track information from Spotify.
+
+
+Installation
+------------
+1. Install Xposed Framework
+2. Install SpotifyAVRCP
+3. Enable SpotifyAVRCP in Xposed
+4. Reboot Phone
+5. Use Spotify as normal, the OS should take care of sending metadata over bluetooth
+
---
+++ SpotifyAVRCP/src/main/java/com/jonasbergler/xposed/spotify/Spotify.java
@@ -0,0 +1,92 @@
+package com.jonasbergler.xposed.spotify;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/**
+ * Created by jbergler on 29/11/2013.
+ */
+public class Spotify implements IXposedHookLoadPackage {
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.spotify.mobile.android.ui"))
+            return;
+
+        Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+        Context context = (Context) callMethod(activityThread, "getSystemContext");
+        PackageInfo pi = context.getPackageManager().getPackageInfo(lpparam.packageName, 0);
+
+        String versionName = pi.versionName;
+        int versionCode = pi.versionCode;
+
+        XposedBridge.log("SpotifyAVRCP: We're in Spotify [" + versionName + "]");
+
+        // Define a big list of know function names for versions here
+        Map<String, String[]> versionToFunction = new HashMap<String, String[]>();
+        versionToFunction.put("0.7.3.636.ga3aacdd3", new String[]{"c", "b"});
+
+        // Lookup the function names for current version
+        String methodName = "c";
+        if (versionToFunction.containsKey(versionName))
+            methodName = versionToFunction.get(versionName)[0];
+
+        findAndHookMethod("com.spotify.mobile.android.service.v", lpparam.classLoader, methodName, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // this will be called before the clock was updated by the original method
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Object paramx = getObjectField(param.thisObject, "b");
+                Service service = (Service) getObjectField(param.thisObject, "a");
+
+                String track = (String) callMethod(paramx, "d");
+                String artist = (String) callMethod(paramx, "f");
+                String album = (String) callMethod(paramx, "e");
+
+                //XposedBridge.log("Spotify track: " + track + " artist: " + artist + " album: " + album);
+
+                Intent localIntent = new Intent("com.android.music.metachanged");
+                localIntent.putExtra("track", track);
+                localIntent.putExtra("artist", artist);
+                localIntent.putExtra("album", album);
+                localIntent.putExtra("playing", true);
+                service.sendBroadcast(localIntent);
+            }
+        });
+
+//        findAndHookMethod("com.spotify.mobile.android.service.v", lpparam.classLoader, "b", new XC_MethodHook() {
+//            @Override
+//            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                Service service = (Service) getObjectField(param.thisObject, "a");
+//
+//                Intent localIntent = new Intent("com.android.music.metachanged");
+//                localIntent.putExtra("track", "");
+//                localIntent.putExtra("artist", "");
+//                localIntent.putExtra("album", "");
+//                localIntent.putExtra("playing", false);
+//                service.sendBroadcast(localIntent);
+//            }
+//        });
+    }
+}
